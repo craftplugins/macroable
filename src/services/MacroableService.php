4f24425a8e81677b0b92d11c0dba61eddfef6843
Joshua Baker <160484+joshuabaker@users.noreply.github.com>
2019-12-18 16:47:57 +0000

    Added support for class usage in config

--- src/services/MacroableService.php
+++ src/services/MacroableService.php
@@ -5,6 +5,9 @@ namespace craftplugins\macroable\services;
 use Craft;
 use craft\base\Component;
 use craftplugins\macroable\library\Collection;
+use ReflectionClass;
+use ReflectionMethod;
+use ReflectionProperty;
 
 /**
  * Class MacroableService
@@ -30,6 +33,8 @@ class MacroableService extends Component
 
     /**
      * @inheritDoc
+     *
+     * @throws \ReflectionException
      */
     public function init()
     {
@@ -37,8 +42,43 @@ class MacroableService extends Component
 
         $config = Craft::$app->config->getConfigFromFile('macroable');
 
-        $this->filters = new Collection($config['filters']);
-        $this->functions = new Collection($config['functions']);
-        $this->globals = new Collection($config['globals']);
+        $this->filters = $this->collect($config['filters']);
+        $this->functions = $this->collect($config['functions']);
+        $this->globals = $this->collect($config['globals']);
+    }
+
+    /**
+     * @param $value
+     *
+     * @return \craftplugins\macroable\library\Collection
+     * @throws \ReflectionException
+     */
+    protected function collect($value): Collection
+    {
+        // We assume strings are instance references
+        if (is_string($value)) {
+            $value = new $value();
+        }
+
+        // Reflect objects into references to methods and properties
+        if (is_object($value)) {
+            $result = [];
+
+            $reflection = new ReflectionClass($value);
+
+            $methods = $reflection->getMethods(ReflectionMethod::IS_PUBLIC);
+            foreach ($methods as $method) {
+                $result[$method->name] = [$value, $method->name];
+            }
+
+            $properties = $reflection->getProperties(ReflectionProperty::IS_PUBLIC);
+            foreach ($properties as $property) {
+                $result[$property->name] = $property->getValue();
+            }
+
+            $value = $result;
+        }
+
+        return new Collection($value);
     }
 }
